ae496be0f4e204d8563de75ef878fe224f06d4a0
Andreas Prang <mail@andreasprang.de>
2016-09-27 00:04:39 +0200

    switched to rpio

--- index.js
+++ index.js
@@ -1,104 +1,77 @@
-// var request = require("request");
-var gpio = require('rpi-gpio');
-var fs = require("fs");
+var rpio = require('rpio');
 var Service, Characteristic;
 
 module.exports = function(homebridge) {
-	Service = homebridge.hap.Service;
-	Characteristic = homebridge.hap.Characteristic;
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
 
-	homebridge.registerAccessory("homebridge-gardendoor", "Gardendoor", DoorAccessory);
+    homebridge.registerAccessory('homebridge-gpio', 'GPIO', GPIOAccessory);
 }
 
-function DoorAccessory(log, config) {
-	this.log = log;
-	this.name = config["name"];
-	this.gpioPort = config["GPIOPort"];
+function GPIOAccessory(log, config) {
+    this.log = log;
+    this.name = config['name'];
+    this.pin = config['pin'];
+    this.duration = config['duration'];
+    this.service = new Service.Switch(this.name);
 
-	this.gardendoorservice = new Service.GarageDoorOpener(this.name);
+    if (!this.pin) throw new Error('You must provide a config value for pin.');
 
-	this.gardendoorservice
-		.getCharacteristic(Characteristic.CurrentDoorState)
-		.on('get', this.getState.bind(this));
+    this.service
+        .getCharacteristic(Characteristic.On)
+        .on('get', this.getOn.bind(this))
+        .on('set', this.setOn.bind(this));
 
-	this.gardendoorservice
-		.getCharacteristic(Characteristic.TargetDoorState)
-		.on('get', this.getState.bind(this))
-		.on('set', this.setState.bind(this));
-
-	this.gardendoorservice
-		.getCharacteristic(Characteristic.ObstructionDetected)
-		.on('get', this.getState.bind(this));
 }
 
-DoorAccessory.prototype.getState = function(callback) {
-	this.log("Getting current state...");
-
-	gpio.read(7, function(err, value) {
-		var closed = (value == false);
-		callback(null, closed); // success
-    });
-/*
-request.get({
-		url: this.statusurl
-	}, function(err, response, body) {
-		if (!err && response.statusCode == 200) {
-			var json = JSON.parse(body);
-			var state = json.state; // "open" or "closed"
-			this.log("Door state is %s", state);
-			var closed = state == "closed"
-			callback(null, closed); // success
-		} else {
-			this.log("Error getting state: %s", err);
-			callback(err);
-		}
-	}.bind(this));
-*/
+GPIOAccessory.prototype.getServices = function() {
+    return [this.service];
 }
 
-DoorAccessory.prototype.setState = function(state, callback) {
-	var doorState = (state == Characteristic.TargetDoorState.CLOSED) ? "closed" : "open";
-	this.log("Set state to %s", doorState);
-	
-	// switch relay to ON
-	gpio.setup(this.gpioPort, gpio.DIR_OUT, function(){
-    	gpio.write(this.gpioPort, true, function(err) {
-        	if (err) throw err;
-        		console.log('Written ON to pin ' + this.gpioPort);
-	    });
-	});
-	
-	setTimeout(
-		function(){  
-			// switch relay to OFF
-			gpio.setup(this.gpioPort, gpio.DIR_OUT, function(){
-	    	gpio.write(this.gpioPort, false, function(err) {
-    	    	if (err) throw err;
-        		console.log('Written OFF to pin ' + this.gpioPort);
-		    });
-		});
-	}, 5000);
-	
-/*	
-	request.get({
-		url: this.controlurl
-	}, function(err, response, body) {
-		if (!err && response.statusCode == 200) {
-			this.log("State change complete.");
-			var currentState = (state == Characteristic.TargetDoorState.CLOSED) ? Characteristic.CurrentDoorState.CLOSED : Characteristic.CurrentDoorState.OPEN;
+GPIOAccessory.prototype.getOn = function(callback) {
 
-			this.gardendoorservice
-			.setCharacteristic(Characteristic.CurrentDoorState, currentState);
+console.log(this.pin);
+        rpio.open(this.pin, rpio.OUTPUT, rpio.LOW);
 
-			callback(null); // success
-		} else {
-			this.log("Error '%s' setting door state. Response: %s", err, body);
-			callback(err || new Error("Error setting door state."));
-		}
-	}.bind(this));
-*/
-},
-
-DoorAccessory.prototype.getServices = function() {
-	return [this.gardendoorservice];
+        var on = rpio.read(this.pin)
+        callback(null, on);
+        console.log('Pin ' + this.pin + ' is currently set ' + (rpio.read(this.pin) ? 'high' : 'low'));
 }
+
+GPIOAccessory.prototype.setOn = function(on, callback) {
+    this.log('GPIOAccessory.prototype.setOn');
+    if (on) {
+        this.pinAction(0);
+                if (is_defined(this.duration) && is_int(this.duration)) {
+                        this.pinTimer()
+                }
+                callback(null);
+    } else {
+                this.pinAction(1);
+                callback(null);
+    }
+}
+
+GPIOAccessory.prototype.pinAction = function(action) {
+        this.log('Turning ' + (action == 0 ? 'on' : 'off') + ' pin #' + this.pin);
+
+        var self = this;
+        rpio.open(this.pin, rpio.OUTPUT);
+        rpio.write(this.pin, action == 0 ? rpio.LOW : rpio.HIGH);
+}
+
+GPIOAccessory.prototype.pinTimer = function() {
+        var self = this;
+        setTimeout(function() {
+                        self.pinAction(1);
+        }, this.duration);
+}
+
+var is_int = function(n) {
+   return n % 1 === 0;
+}
+
+var is_defined = function(v) {
+        return typeof v !== 'undefined';
+}
+
