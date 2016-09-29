6ff759075ca5a0a68d4f2ea7c943f4b617392f6c
Andreas Prang <mail@andreasprang.de>
2016-09-29 22:09:54 +0200

    Changed from switch to DoorLock

--- index.js
+++ index.js
@@ -5,42 +5,48 @@ module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
 
-    homebridge.registerAccessory('homebridge-gpio', 'GPIO', GPIOAccessory);
+    homebridge.registerAccessory('homebridge-gpio', 'GPIO', LockitronAccessory);
 }
 
-function GPIOAccessory(log, config) {
+function LockitronAccessory(log, config) {
     this.log = log;
     this.name = config['name'];
     this.pin = config['pin'];
     this.duration = config['duration'];
-    this.service = new Service.Switch(this.name);
+    this.service = new Service.LockMechanism(this.name);
 
     if (!this.pin) throw new Error('You must provide a config value for pin.');
 
     this.service
-        .getCharacteristic(Characteristic.On)
-        .on('get', this.getOn.bind(this))
-        .on('set', this.setOn.bind(this));
+    .getCharacteristic(Characteristic.LockCurrentState)
+    .on('get', this.getState.bind(this));
 
+    this.service
+    .getCharacteristic(Characteristic.LockTargetState)
+    .on('get', this.getState.bind(this))
+    .on('set', this.setState.bind(this));
 }
 
-GPIOAccessory.prototype.getServices = function() {
+LockitronAccessory.prototype.getServices = function() {
     return [this.service];
 }
 
-GPIOAccessory.prototype.getOn = function(callback) {
+LockitronAccessory.prototype.getState = function(callback) {
+        this.log("Getting current state...");
 
-console.log(this.pin);
-        rpio.open(this.pin, rpio.OUTPUT, rpio.LOW);
+        rpio.open(this.pin, rpio.OUTPUT);
 
         var on = rpio.read(this.pin)
         callback(null, on);
         console.log('Pin ' + this.pin + ' is currently set ' + (rpio.read(this.pin) ? 'high' : 'low'));
 }
 
-GPIOAccessory.prototype.setOn = function(on, callback) {
-    this.log('GPIOAccessory.prototype.setOn');
-    if (on) {
+LockitronAccessory.prototype.setState = function(state, callback) {
+  var lockitronState = (state == Characteristic.LockTargetState.SECURED) ? "lock" : "unlock";
+
+  this.log("Set state to %s", lockitronState);
+
+    if (state == Characteristic.LockTargetState.UNSECURED) {
         this.pinAction(0);
                 if (is_defined(this.duration) && is_int(this.duration)) {
                         this.pinTimer()
@@ -52,7 +58,7 @@ GPIOAccessory.prototype.setOn = function(on, callback) {
     }
 }
 
-GPIOAccessory.prototype.pinAction = function(action) {
+LockitronAccessory.prototype.pinAction = function(action) {
         this.log('Turning ' + (action == 0 ? 'on' : 'off') + ' pin #' + this.pin);
 
         var self = this;
@@ -60,7 +66,7 @@ GPIOAccessory.prototype.pinAction = function(action) {
         rpio.write(this.pin, action == 0 ? rpio.LOW : rpio.HIGH);
 }
 
-GPIOAccessory.prototype.pinTimer = function() {
+LockitronAccessory.prototype.pinTimer = function() {
         var self = this;
         setTimeout(function() {
                         self.pinAction(1);
@@ -74,4 +80,3 @@ var is_int = function(n) {
 var is_defined = function(v) {
         return typeof v !== 'undefined';
 }
-
